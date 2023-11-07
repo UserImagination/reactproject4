55045044f577c84f5bebad87998e6debd8f53f1e
hawk2 <joehaven@gmail.com>
2023-11-07 14:36:43 -0800

    now we have a definition card meow

--- src/Components/originalPage.jsx
+++ src/Components/originalPage.jsx
@@ -1,7 +1,13 @@
 
 import axios from "axios";
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+
 
 class OriginalPage extends React.Component {
   constructor(props) {
@@ -16,44 +22,44 @@ class OriginalPage extends React.Component {
     this.getDef = this.getDef.bind(this);
   }
 
-// *************************** //
-// ******** NUM STATE ******** //
-// *************************** //
+  // *************************** //
+  // ******** NUM STATE ******** //
+  // *************************** //
 
-//const [num, setNum] = useState(0); // the 0 is the intial value of num, ie let num=0
-// 
+  //const [num, setNum] = useState(0); // the 0 is the intial value of num, ie let num=0
+  // 
   increaseNum = () => {
-    this.setState({num: this.state.num + 1});
+    this.setState({ num: this.state.num + 1 });
   };
   decreaseNum = () => {
-    this.setState({num: this.state.num - 1});
+    this.setState({ num: this.state.num - 1 });
   }
 
-// *************************** //
-// ******** INPUT STATE ****** //
-// *************************** //
-//const [inputValue, setInputValue] = useState(""); // empty string
+  // *************************** //
+  // ******** INPUT STATE ****** //
+  // *************************** //
+  //const [inputValue, setInputValue] = useState(""); // empty string
   handleInputChange = (event) => {
     console.log(event.target.value);
-    this.setState({inputValue: event.target.value});
+    this.setState({ inputValue: event.target.value });
   };
 
-// *************************** //
-// ******** HIDE STATE ******* //
-// *************************** //
-//const [showText, setShowText] = useState(true); // boolean
-toggleText = () => {
-  if(this.state.show === true) {
-    this.setState({show: false})
+  // *************************** //
+  // ******** HIDE STATE ******* //
+  // *************************** //
+  //const [showText, setShowText] = useState(true); // boolean
+  toggleText = () => {
+    if (this.state.show === true) {
+      this.setState({ show: false })
+    }
+    if (this.state.show === false) {
+      this.setState({ show: true })
+    }
   }
-  if(this.state.show === false) {
-    this.setState({show: true})
-  }
-}
 
-// *************************** //
-// ******** GET DEF ********** //
-// *************************** //
+  // *************************** //
+  // ******** GET DEF ********** //
+  // *************************** //
   async getDef(search) {
     search = document.getElementById("searchWord").value;
     console.log(search);
@@ -70,9 +76,9 @@ toggleText = () => {
       const response = await axios.request(options);
 
       axios.request(options).then((response) => {
-        this.setState({data: response.data}) 
-       })
-      
+        this.setState({ data: response.data })
+      })
+
       if (response.status === 200) {
         // Data is available; set it in the state
         this.setState({ data: response.data });
@@ -86,21 +92,21 @@ toggleText = () => {
     } catch (error) {
       console.error('An error occurred:', error);
     }
-  
-   
-      // * MOVED TO INSIDE OF TRY * //
-      // axios.request(options).then((response) => {
-      //  this.setState({data: response.data}) 
-      // })
+
+
+    // * MOVED TO INSIDE OF TRY * //
+    // axios.request(options).then((response) => {
+    //  this.setState({data: response.data}) 
+    // })
 
   }
-// *************************** //
-// ******* INPUT STATE 2****** //
-// *************************** //
-//const [inputValue2, setInputValue2] = useState(""); // empty string
+  // *************************** //
+  // ******* INPUT STATE 2****** //
+  // *************************** //
+  //const [inputValue2, setInputValue2] = useState(""); // empty string
   handleInputChange2 = (event) => {
     console.log(event.target.value);
-    this.setState({searchTerm: event.target.value});
+    this.setState({ searchTerm: event.target.value });
   };
 
   render() {
@@ -116,7 +122,7 @@ toggleText = () => {
         </p>
 
         <p>
-          <input type="text" onChange={this.handleInputChange}/>
+          <input type="text" onChange={this.handleInputChange} />
           <p>{this.state.inputValue}</p>
         </p>
 
@@ -131,13 +137,55 @@ toggleText = () => {
           <button onClick={this.toggleText}>Show/Hide</button>
           {this.state.show && <h1>MEOW</h1>}
         </p>
+        
+
+        
+
+
+        
+
+
+
+    <div style={{margin: '25%'}}>
+    <Card sx={{ maxWidth: 600 }}>
+      <CardContent>
+        <Typography variant='h3'  gutterBottom>
+          OK!!!
+        </Typography>
+        <Typography variant="h4" component="div">
         <p>
           <input id="searchWord" type="text" onChange={this.handleInputChange2}></input>
         </p>
-
         <p>
           <button onClick={this.getDef}>Get Definition</button>
         </p>
+        </Typography>
+        {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          definition
+        </Typography> */}
+        <Typography variant="body1">
+          <div>          
+          {/* <h2>Definition:</h2> */}
+          {this.state.data && (
+            <ul>
+              {this.state.data.definitions.map((definition, index) => (
+                <li key={index}>{definition.definition}</li>
+              ))}
+            </ul>           
+          )}        
+        </div>
+          <br />
+          {'"very good"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Card Button</Button>
+      </CardActions>
+    </Card>
+    </div>
+  );
+
+
         <Link to='/page2'>link to another page</Link>
       </div>
     )
