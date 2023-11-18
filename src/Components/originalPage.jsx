0e36c4d13db326e88b070359798151eb56dc4e70
Matthew Craig <mgcraig78@gmail.com>
2023-11-18 00:12:44 -0800

    no message

--- src/Components/originalPage.jsx
+++ src/Components/originalPage.jsx
@@ -2,11 +2,12 @@
 import axios from "axios";
 import React from 'react';
 import { Link } from "react-router-dom";
-import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
-import CardContent from '@mui/material/CardContent';
+
+
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
+import { Grid, CardContent, Typography, Card } from '@mui/material';
+
 import './originalPage.css'; 
 
 
@@ -19,22 +20,6 @@ class OriginalPage extends React.Component {
       searchTerm: '',
       inputValue: '',
       show: true,
-      testDefinitions: [{
-        definition: 'definition number one',
-        word: 'word number 1',
-        id: 1
-        },
-        {
-        definition: 'definition number two',
-        word: 'word number 2',
-        id: 2
-        },
-        {
-        definition: 'definition number three',
-        word: 'word number 3',
-        id: 3
-        },
-      ]
     }
     this.getDef = this.getDef.bind(this);
   }
@@ -90,17 +75,21 @@ class OriginalPage extends React.Component {
 
     try {
       const response = await axios.request(options);
-
+      let result = [];
+      if (this.state.data && this.state.data.length > 0) {
+        result.push(...this.state.data)
+      }
       axios.request(options).then((response) => {
-        this.setState(prevState => {
-          return {
-            ...prevState,
-            data: response.data,
-          }})
-        
-        //this.mapFunction(response.data);
+        result.push(response.data)
+        if(result.length > 0) {
+          this.setState(prevState => {
+            return {
+              ...prevState,
+              data: result,
+            }
+          })
+        }
       })
-
       if (response.status === 200) {
         // Data is available; set it in the state
         this.setState({ data: response.data });
@@ -130,6 +119,9 @@ class OriginalPage extends React.Component {
     this.setState({ searchTerm: event.target.value });
   };
 
+  debugger = () => {
+    debugger;
+  }
   render() {
     return (
       <div>
@@ -159,38 +151,6 @@ class OriginalPage extends React.Component {
           {this.state.show && <h1>MEOW</h1>}
         </p>
         
-        {/*<div className='centered'>*/}
-        {/*  <Card className='width'>*/}
-        {/*    <CardContent>*/}
-        {/*      <Typography variant='h3'  gutterBottom>*/}
-        {/*        OK!!!*/}
-        {/*      </Typography>*/}
-        {/*      <Typography variant="h4" component="div">*/}
-        {/*      </Typography>*/}
-        {/*      /!* <Typography sx={{ mb: 1.5 }} color="text.secondary">*/}
-        {/*  definition*/}
-        {/*</Typography> *!/*/}
-        {/*      <Typography variant="body1">*/}
-        {/*        <div>*/}
-        {/*          /!* <h2>Definition:</h2> *!/*/}
-        {/*          {this.state.data && (*/}
-        {/*            <ul>*/}
-        {/*              {this.state.data.definitions.map((definition, index) => (*/}
-        {/*                <li key={index}>{definition.definition}</li>*/}
-        {/*              ))}*/}
-        {/*            </ul>*/}
-        {/*          )}*/}
-        {/*        </div>*/}
-        {/*        <br />*/}
-        {/*        {'"very good"'}*/}
-        {/*      </Typography>*/}
-        {/*    </CardContent>*/}
-        {/*    <CardActions>*/}
-        {/*      <Button size="small">Card Button</Button>*/}
-        {/*    </CardActions>*/}
-        {/*  </Card>*/}
-        {/*</div>*/}
-        
         <p>
           <input id="searchWord" type="text" onChange={this.handleInputChange2}></input>
         </p>
@@ -198,21 +158,37 @@ class OriginalPage extends React.Component {
           <button onClick={this.getDef}>Get Definition</button>
         </p>
 
-        {/*update this to map cards!!!!*/}
-        <div className='centered shadow'>
-        <h2>Map in Template</h2>
-          <ul className='width'>
-            {/*maps through it on page load*/}
-            {this.state.testDefinitions.map(function(item) {
-              return (
-                  // each item being mapped needs a key, which can also be passed in the above, after 'item'... function(item, i)...
-                  <li key={item.id}>{item.definition}</li>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 6, sm: 2, md: 4 }}>
+            {this.state.data && (this.state.data.length > 0) && this.state.data.map(function(iterator, index) {
+               return (
+                 <Grid item xs={6}>
+                    <div className='centered' key={index}>
+                      <Card className='width'>
+                        <CardContent>
+                          <Typography variant='h5'  gutterBottom>
+                            Word: {iterator.word}
+                          </Typography>
+                          <Typography variant="h6" component="div">
+                            Definition(s):
+                          </Typography>
+                          <div>
+                            <ul>
+                            {iterator.definitions.map(function(item, index) {
+                              return(<li key={index}>{item.definition}</li>)
+                            })
+                            }
+                            </ul>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    </div>
+                 </Grid>
                 )
               })
             }
-          </ul>
-        </div>
+        </Grid>
         <Link to='/page2'>link to another page</Link>
+        {/*<button onClick={this.debugger}>Deboooger</button>*/}
       </div>
     )
   }
