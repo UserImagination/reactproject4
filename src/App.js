7e6b6c857745bd0f8f7f15bb9c97d4c278e6abd5
Andres Guerrero <workconnector@gmail.com>
2023-12-04 17:40:44 -0600

    enable if condition App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./Components/Navbar"
 import ContactPage from "./Components/contactPage";
 import LoginPage from "./Components/loginPage";
 import ShopPage from "./Components/shopPage";
+import Register from './Register';
 //import BlogPage from ".Components/blogPage;"
 
 // a state is like a variable that stores data
@@ -13,9 +14,9 @@ import ShopPage from "./Components/shopPage";
 const App = () => {
   const [token, setToken] = useState();
   
-  // if(!token) {
-  //   return <LoginPage setToken={setToken} />  //This function checks if the user has already logged in
-  // }
+   if(!token) {
+    return <Register setToken={setToken} />  //This function checks if the user has already logged in
+   }
   
   const username = (data) => {
     console.log(data);
--- src/login.js
+++ src/login.js
@@ -2,8 +2,7 @@ const Login = () => {
     
         return(
             <div>
-
-      
+                
             </div>
         )
     }
